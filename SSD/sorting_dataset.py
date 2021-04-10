d9c756f51a556e4622c259a39e8dd9374cba7d8c
mariusud <mariusudnaes@gmail.com>
2021-04-10 13:34:39 +0000

    ssd512 with 41.68% mAP

--- SSD/sorting_dataset.py
+++ SSD/sorting_dataset.py
@@ -4,35 +4,70 @@ import glob
 import sys
 import shutil
 import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
+import matplotlib.pyplot as plt
 
-# for file in glob.glob(os.path.join("SSD/datasets/RDD2020_filtered/Annotations", "*.xml")):
-#     # open xml
+# for file in glob.glob(os.path.join("datasets/RDD2020_filtered/Annotations", "*.xml")):
 #     tree = ET.parse(file)
 #     root = tree.getroot()
-#     object_root = root.find("object")
-#     label = object_root.find("name")
-#     print(label.text)
-#     # print(str(file).split('\\')[1].lstrip().split('.')[0])
-#     shutil.copy(file,"SSD/datasets/RDD2020_filtered/%s/Annotations" % label.text)
-#     shutil.copy(os.path.join("SSD/datasets/RDD2020_filtered/JPEGImages/%s.jpg" % (str(file).split('\\')[1].lstrip().split('.')[0])), "SSD/datasets/RDD2020_filtered/%s/JPEGImages" % label.text)
+#     for node in tree.findall('.//object/name'):
+#         label = node.text
+#         shutil.copy(file,"datasets/RDD2020_filtered/%s/Annotations" % label)
+#         shutil.copy(os.path.join("datasets/RDD2020_filtered/JPEGImages/%s.jpg" % (str(file).split('\\')[1].lstrip().split('.')[0])), "datasets/RDD2020_filtered/%s/JPEGImages" % label)
 
 delta_x = []
 delta_y = []
-for file in glob.glob(os.path.join("SSD/datasets/RDD2020_filtered/D40/Annotations", "*.xml")):
-    # open xml
+area = []
+for file in glob.glob(os.path.join("datasets/RDD2020_filtered/D40/Annotations", "*.xml")):
     tree = ET.parse(file)
     root = tree.getroot()
-    object_root = root.find("object")
-    bndbox = object_root.find("bndbox")
-    xmin = int(bndbox.find("xmin").text)
-    ymin = int(bndbox.find("ymin").text)
-    xmax = int(bndbox.find("xmax").text)
-    ymax = int(bndbox.find("ymax").text)
-    delta_x.append(xmax-xmin)
-    delta_y.append(ymax-ymin)
+    for node in tree.findall('.//object'):
+        if (node.find("name").text=="D40"):
+            bndbox = node.find("bndbox")
+            xmin = int(bndbox.find("xmin").text)
+            ymin = int(bndbox.find("ymin").text)
+            xmax = int(bndbox.find("xmax").text)
+            ymax = int(bndbox.find("ymax").text)
+            # if(xmax-xmin<600 and ymax-ymin<600):
+            delta_x.append(xmax-xmin)
+            delta_y.append(ymax-ymin)
+            area.append((xmax-xmin)*(ymax-ymin))
+            # if(xmax-xmin>600 or ymax-ymin>600):
+            #     print (file)
 
-mean_delta_x = np.mean(np.asarray(delta_x))
-mean_delta_y = np.mean(np.asarray(delta_y))
+delta_x = np.asarray(delta_x)
+delta_y = np.asarray(delta_y)
+area = np.asarray(area)
 
-print(mean_delta_x)
-print(mean_delta_y)
+# min_pos_x = np.argmin(delta_x)
+# min_pos_y = np.argmin(delta_y)
+# min_x = np.min(delta_x)
+# min_y = np.min(delta_y)
+# print('minx:',min_x,delta_y[min_pos_x])
+# print('miny:',delta_x[min_pos_y],min_y)
+
+# max_pos_x = np.argmax(delta_x)
+# max_pos_y = np.argmax(delta_y)
+# max_x = np.max(delta_x)
+# max_y = np.max(delta_y)
+# print('maxx:',max_x,delta_y[max_pos_x])
+# print('maxy:',delta_x[max_pos_y],max_y)
+
+# mean_delta_x = np.mean(delta_x)
+# mean_delta_y = np.mean(delta_y)
+# new_array = np.vstack((delta_x,delta_y))
+
+# print(mean_delta_x)
+# print(mean_delta_y)
+
+# ratio_vector = np.zeros(np.shape(new_array)[1])
+# for i in range(np.shape(new_array)[1]):
+#     ratio_vector[i] = round(new_array[0][i]/new_array[1][i])
+# _, counts = np.unique(ratio_vector,return_counts=True)
+# plt.bar(np.arange(len(counts)), counts)
+# print(counts)
+# plt.show()
+print(np.shape(area))
+plt.hist(area)
+plt.show()
+
--- SSD/ssd/modeling/backbone/ssd512.py
+++ SSD/ssd/modeling/backbone/ssd512.py
@@ -7,6 +7,8 @@ from torchvision.models.resnet import resnet18, resnet34, resnet50, resnet101, r
 # https://github.com/NVIDIA/DeepLearningExamples/blob/master/PyTorch/Detection/SSD/src/model.py
 
 
+#loosely based on https://github.com/kuangliu/torchcv/blob/6291f3e1e4bbf6467fd6b1e79001d34a59481bb6/torchcv/models/ssd/net.py#L146
+
 class Mish(nn.Module):
     def __init__(self):
         super().__init__()
@@ -47,7 +49,7 @@ class SSD512(nn.Module):
         image_channels = cfg.MODEL.BACKBONE.INPUT_CHANNELS
         self.output_feature_shape = cfg.MODEL.PRIORS.FEATURE_MAPS
 
-        #self.l2_norm = L2Norm(1024, scale=20)
+        self.l2_norm = L2Norm(512, scale=20)
 
         '''
         # Extracting the the required layers form the backbone
@@ -73,7 +75,7 @@ class SSD512(nn.Module):
         conv4_block1.conv1.stride = (1,1)
         conv4_block1.conv2.stride = (1,1)
         conv4_block1.downsample[0].stride = (1,1)
-        self.bank1.add_module("conv2d",nn.Conv2d(1024,512, kernel_size=1))
+        self.bank1.add_module("conv5_1",nn.Conv2d(1024,512, kernel_size=1))
         
         self.conv12_1 = nn.Conv2d(256, 128, kernel_size=1)
         self.conv12_2 = nn.Conv2d(128, 256, kernel_size=4, padding=1)
@@ -81,8 +83,8 @@ class SSD512(nn.Module):
         self.bank8 = nn.Sequential(self.conv12_1, self.conv12_2)
 
         self.bank2 = self._build_layer(self.output_channels[0], self.output_channels[1],2, 512)
-        self.bank3 = self._build_layer(self.output_channels[1], self.output_channels[2],3, 256)
-        self.bank4 = self._build_layer(self.output_channels[2], self.output_channels[3],4, 128)
+        self.bank3 = self._build_layer(self.output_channels[1], self.output_channels[2],3, 1024)
+        self.bank4 = self._build_layer(self.output_channels[2], self.output_channels[3],4, 256)
         self.bank5 = self._build_layer(self.output_channels[3],self.output_channels[4],5, 128)
         self.bank6 = self._build_layer(self.output_channels[4],self.output_channels[5],6, 128)
         self.bank7 = self._build_layer(self.output_channels[5],self.output_channels[6],7, 128, stride=1,padding=0)
@@ -90,7 +92,6 @@ class SSD512(nn.Module):
         self.feature_extractor = nn.ModuleList([self.bank1, self.bank2, self.bank3, self.bank4, self.bank5, self.bank6, self.bank8])
     
     def _build_layer(self, input_size,output_size, layer_no, channels, stride=2, padding=1, dilation=0):
-        print("INPUT SIZE: %s, OUTPUT SIZE: %s" % (input_size, output_size) )
         layer = nn.Sequential(
                 nn.Conv2d(
                     in_channels = input_size,
@@ -113,6 +114,31 @@ class SSD512(nn.Module):
             
                 )
         return layer
+    
+    def _build_fat_layer(self, input_size,output_size, layer_no, channels, stride=2, padding=1, dilation=0):
+        layer = nn.Sequential(
+                nn.Conv2d(
+                    in_channels = input_size,
+                    out_channels = channels,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0
+                ),
+                nn.BatchNorm2d(channels),
+                Mish(),
+                nn.Conv2d(
+                    in_channels = channels,
+                    out_channels = output_size,
+                    kernel_size=3,
+                    stride=stride,
+                    padding=padding
+                ),
+                nn.BatchNorm2d(output_size),
+                Mish(),
+            
+                )
+        return layer
+    
     
     def forward(self, x):
         out = []
