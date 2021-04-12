a7b28fdd26cc30cafa0955de6794d4133117a0b0
mariusud <mariusudnaes@gmail.com>
2021-04-12 11:24:02 +0000

    update with ssd512

--- SSD/ssd/modeling/detector.py
+++ SSD/ssd/modeling/detector.py
@@ -5,13 +5,9 @@ from ssd.modeling.box_head.box_head import SSDBoxHead
 from ssd.utils.model_zoo import load_state_dict_from_url
 from ssd import torch_utils
 import torchvision.models as models
-<<<<<<< HEAD
 
-#from ssd.modeling.backbone.test_resnet import ResNet
-=======
->>>>>>> 219c6bae3a4ec91a94d71a7ef5129c288972d337
 from ssd.modeling.backbone.resnet_experimental import ResNet_Experimental
-
+from ssd.modeling.backbone.ssd512 import SSD512
 
 
 class SSDDetector(nn.Module):
@@ -41,6 +37,8 @@ def build_backbone(cfg):
     if backbone_name == "basic":
         model = Second_Improved_BasicModel(cfg)
         return model
+    if backbone_name == "SSD512":
+        return SSD512(cfg)
     if backbone_name == "resnet_experimental":
         return ResNet_Experimental(cfg)
     if backbone_name == "vgg":
